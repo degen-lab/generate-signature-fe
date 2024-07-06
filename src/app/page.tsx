1e355b13c3d11d6181dcc90f4ead6f665e4f5c1b
BowTiedBlox <bowtiedblox@gmail.com>
2024-07-06 17:49:25 +0300

    update landing to fit subdomain & npm packages versions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { Landing } from "./components/landing/Landing";
 import { Background } from "./components/background/Background";
+import { SignaturePage } from "./components/signature/signature-page/SignaturePage";
 
 export default function Home() {
   return (
     <>
       <Background />
       <div className="relative h-[100vh] flex justify-center items-center flex-col">
-        <Landing />
+        <SignaturePage />
       </div>
     </>
   );
