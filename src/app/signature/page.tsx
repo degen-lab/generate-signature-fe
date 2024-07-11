0e915c7f27b55995e952a9a3dda10e3b61c53d5a
BowTiedBlox <bowtiedblox@gmail.com>
2024-07-12 00:28:47 +0300

    fix: remove unused module for background

--- src/app/signature/page.tsx
+++ src/app/signature/page.tsx
@@ -1,11 +1,5 @@
-import { Background } from "../components/background/Background";
 import { SignaturePage } from "../components/signature/signature-page/SignaturePage";
 
 export default function Signature() {
-  return (
-    <>
-      <Background />
-      <SignaturePage></SignaturePage>
-    </>
-  );
+  return <SignaturePage />;
 }
